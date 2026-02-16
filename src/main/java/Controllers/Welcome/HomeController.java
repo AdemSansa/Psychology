6117d15f0da56985c742d3e5745ddcbd92c8ebdb
Rabii khenissi <rabiikhenissi124@gmail.com>
2026-02-16 03:11:45 +0100

    fixed sidebar

--- src/main/java/Controllers/Welcome/HomeController.java
+++ src/main/java/Controllers/Welcome/HomeController.java
@@ -1,29 +1,41 @@
 package Controllers.Welcome;
 
 import javafx.fxml.FXML;
+import javafx.scene.layout.StackPane;
 import util.SceneManager;
 
 public class HomeController {
 
+    @FXML
+    private StackPane contentArea;
 
     @FXML
-    public void gotoFeatures() {
-        SceneManager.switchScene("/com/example/psy/intro/features.fxml");
+    public void initialize() {
+
+        // Tell SceneManager where pages should load
+        SceneManager.setContentArea(contentArea);
+
+        // Default page
+        SceneManager.loadPage("/com/example/psy/intro/dashboard.fxml");
     }
 
     @FXML
     public void gotoUsers() {
-        SceneManager.switchScene("/com/example/psy/User/users.fxml");
+        SceneManager.loadPage("/com/example/psy/User/users.fxml");
     }
 
     @FXML
     public void gotoEvents() {
-        SceneManager.switchScene("/com/example/psy/Event/events.fxml");
+        SceneManager.loadPage("/com/example/psy/Event/events.fxml");
     }
 
     @FXML
-    public void gotoTherapists() { SceneManager.switchScene("/com/example/psy/Therapist/therapist_crud.fxml"); }
+    public void gotoTherapists() {
+        SceneManager.loadPage("/com/example/psy/Therapist/therapist_crud.fxml");
+    }
 
     @FXML
-    public void gotoAppoitnments() { SceneManager.switchScene("/com/example/psy/Appointment/AppointmentCalendar.fxml"); }
+    public void gotoAppoitnments() {
+        SceneManager.loadPage("/com/example/psy/Appointment/AppointmentCalendar.fxml");
+    }
 }
